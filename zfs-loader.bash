db88b916937282a2bbb69fd604f5604dd7766057
Levi <alex.garay94@gmail.com>
2025-03-08 23:36:21 -0800

    Update zfs-loader.bash

--- zfs-loader.bash
+++ zfs-loader.bash
@@ -1,16 +1,20 @@
 pacman -Sy
 
-pacman -S neovim --noconfirm
+pacman -S neovim wget --noconfirm
 
-curl -LO https://github.com/n0xena/archzfs/releases/download/2.3.0_6.13.5-dkms/zfs-linux-2.3.0_6.13.5.arch1.1-1-x86_64.pkg.tar.zst
-curl -LO https://github.com/n0xena/archzfs/releases/download/2.3.0_6.13.5-dkms/zfs-linux-headers-2.3.0_6.13.5.arch1.1-1-x86_64.pkg.tar.zst
-curl -LO https://github.com/n0xena/archzfs/releases/download/2.3.0_6.13.5-dkms/zfs-utils-2.3.0-1-x86_64.pkg.tar.zst
+mkdir zfs-files
 
-pacman -U zfs-linux-headers-*.pkg.tar.zst zfs-linux-*.pkg.tar.zst zfs-utils-*.pkg.tar.zst --noconfirm
+wget -P ./zfs-files/ https://github.com/n0xena/archzfs/releases/download/2.3.0_6.13.5-dkms/zfs-linux-2.3.0_6.13.5.arch1.1-1-x86_64.pkg.tar.zst
+wget -P ./zfs-files/ https://github.com/n0xena/archzfs/releases/download/2.3.0_6.13.5-dkms/zfs-linux-headers-2.3.0_6.13.5.arch1.1-1-x86_64.pkg.tar.zst
+wget -P ./zfs-files/ https://github.com/n0xena/archzfs/releases/download/2.3.0_6.13.5-dkms/zfs-utils-2.3.0-1-x86_64.pkg.tar.zst
+
+pacman -U ~/zfs-files/*
 
 modprobe zfs
 
 lsmod | grep zfs
+
+cryptsetup open /dev/nvme0n1p2 cryptroot
 
 zpool import -N -R /mnt think
 zfs mount think/root
